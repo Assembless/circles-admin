ba6ee5a51ecdcfbd42f0571ac7d31a72ff288b47
norwegianbeginner <r.cichocki.dcm@gmail.com>
2021-02-18 18:58:52 +0100

    :recycle: Add component states

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import { TextField, Box } from '@material-ui/core';
+import React, { useState } from 'react'
+import { TextField, Box, Button } from '@material-ui/core';
 import cx from "classnames";
 import useStyles from "./styles"
 
 const Login = () => {
     const classes = useStyles();
 
+    const [login, setLogin] = useState();
+    const [password, setPassword] = useState();
+
+    const handleChange = (e: any) => {
+        const value = e.target.value
+        const id = e.target.id;
+        switch (id) {
+            case 'login':
+                setLogin(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+        }
+    }
+
+    const handleLogin = () => {
+        console.log('login')
+    }
+
     return (
         <div className={classes.root}>
             <div className={classes.loginContainer}>
                 <h2>Circles Administration Panel</h2>
-                <TextField className={classes.login} id="label" label="Label" variant="outlined" />
-                <TextField className={classes.password} id="label" label="Label" variant="outlined" />
+                <TextField className={classes.login} id="login" label="login" variant="outlined" value={login} onChange={handleChange} />
+                <TextField className={classes.password} id="password" label="Password" variant="outlined" value={password} onChange={handleChange} />
+                <Button variant='contained' onClick={handleLogin}>login</Button>
             </div>
         </div>
     )
