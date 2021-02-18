4950f67f866f5a12dd65f992772f8adc7fb8f0fb
norwegianbeginner <r.cichocki.dcm@gmail.com>
2021-02-18 20:50:41 +0100

    :recycle: Add submit form handling

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
 import { TextField, Box, Button } from '@material-ui/core';
-import cx from "classnames";
+import { Alert, AlertTitle } from '@material-ui/lab';
+import { useHistory } from 'react-router-dom';
 import useStyles from "./styles"
 
 const Login = () => {
+
+    const history = useHistory();
     const classes = useStyles();
 
-    const [login, setLogin] = useState();
-    const [password, setPassword] = useState();
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState(false);
 
     const handleChange = (e: any) => {
         const value = e.target.value
@@ -15,27 +19,52 @@ const Login = () => {
         switch (id) {
             case 'login':
                 setLogin(value);
+                setError(false);
                 break;
             case 'password':
                 setPassword(value);
+                setError(false);
                 break;
         }
     }
 
-    const handleLogin = () => {
-        console.log('login')
+    const { REACT_APP_LOGIN, REACT_APP_PASSWORD } = process.env
+
+    const handleSubmit = () => {
+        if (login === REACT_APP_LOGIN && password === REACT_APP_PASSWORD) {
+            history.push('/');
+        }
+        else {
+            setError(true);
+        }
     }
 
     return (
         <div className={classes.root}>
-            <div className={classes.loginContainer}>
+            <form className={classes.loginContainer}>
                 <h2>Circles Administration Panel</h2>
-                <TextField className={classes.login} id="login" label="login" variant="outlined" value={login} onChange={handleChange} />
-                <TextField className={classes.password} id="password" label="Password" variant="outlined" value={password} onChange={handleChange} />
-                <Button variant='contained' onClick={handleLogin}>login</Button>
-            </div>
+                <Box>
+                    <TextField className={classes.login} id="login" label="Login" variant="outlined" value={login} onChange={handleChange} />
+                </Box>
+                <Box>
+                    <TextField className={classes.password} type='password' id="password" label="Password" variant="outlined" value={password} onChange={handleChange} />
+                </Box>
+                <Button variant='contained' onClick={handleSubmit}>login</Button>
+            </form>
         </div>
     )
 }
-
+/* 
+const DisplayError = ({ fieldName, error, value }: { fieldName: string, error: boolean, value: string }) => {
+    if (!error) return null;
+    return (<>
+        {error && !value && <Alert style={{ margin: " 15px 0" }} severity="error">
+            <AlertTitle>Upps...</AlertTitle>
+            {fieldName} is required</Alert>}
+        {error && value && <Alert style={{ margin: " 15px 0" }} severity="error">
+            <AlertTitle>Upps...</AlertTitle>
+            {fieldName} wrong value</Alert>}
+    </>)
+}
+ */
 export default Login;
